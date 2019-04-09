9aa178c73826c469b41bdd10bcb61c5e0de99cd8
1809591 <1809591@rgu.ac.uk>
2019-04-09 17:16:09 +0100

    first templates

---
+++ PHP_Scripts/admin_edit_user.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1809591
+ * Date: 04/04/2019
+ * Time: 19:05
+ */
---
+++ templates/driver_nav_bar.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1809591
+ * Date: 09/04/2019
+ * Time: 17:11
+ */
