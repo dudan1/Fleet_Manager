1a30f6c080216125a36f480400cc4022bce0dc2c
1809591 <1809591@rgu.ac.uk>
2019-04-22 16:21:44 +0100

    Finished

--- PHP_Scripts/admin_edit_driver.php
+++ PHP_Scripts/admin_edit_driver.php
@@ -5,21 +5,24 @@ require 'db_connect.php';
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
-$stmt = $connection->prepare("UPDATE drivers SET first_name = ?, surname = ?, vehicle_reg = ?, licence_number = ?, licence_points = ? WHERE email = ?");
-$stmt->bind_param("sssssi", $first_name, $surname, $vehicle_reg, $licence_number,$licence_points, $email);
 
-$email = $_POST['email'];
-$first_name = mysqli_real_escape_string($connection, $_POST['first_name']);
-$surname = mysqli_real_escape_string($connection, $_POST['surname']);
-$vehicle_reg = mysqli_real_escape_string($connection, $_POST['registration']);
-$licence_number = mysqli_real_escape_string($connection, $_POST['licence']);
-$licence_points = mysqli_real_escape_string($connection, $_POST['points']);
+if($stmt = $connection->prepare("UPDATE drivers SET first_name = ?, surname = ?, vehicle_reg = ?, licence_number = ?, licence_points = ? WHERE email = ?")) {
+   if($stmt->bind_param("ssssis", $first_name, $surname, $vehicle_reg, $licence_number, $licence_points, $email)){
 
-$stmt->execute();
+    $email = $_POST['email'];
+    $first_name = mysqli_real_escape_string($connection, $_POST['first_name']);
+    $surname = mysqli_real_escape_string($connection, $_POST['surname']);
+    $vehicle_reg = mysqli_real_escape_string($connection, $_POST['registration']);
+    $licence_number = mysqli_real_escape_string($connection, $_POST['licence']);
+    $licence_points = mysqli_real_escape_string($connection, $_POST['points']);
+    if($stmt->execute()){
+    echo "New records created successfully";
 
-echo "New records created successfully";
+    $stmt->close();
+    $connection->close();
+    header("Location:../admin_manage_driver.php");
+}
+}
+}
 
-$stmt->close();
-$connection->close();
-header("Location:../admin_manage_driver.php");
 ?>
